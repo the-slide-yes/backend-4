07a56e7e9665ce1470804ebf9fb11c9f6b2f67b8
Aburnett2-rrc <aburnett2@Academic.RRC.CA>
2025-10-20 17:01:18 -0500

    feat: add custom error classes

--- src/constants/httpConstants.ts
+++ src/constants/httpConstants.ts
@@ -6,6 +6,8 @@ export const HTTP_STATUS: {
     CREATED: number,
     BAD_REQUEST: number,
     NOT_FOUND: number,
+    UNAUTHORIZED: number,
+    FORBIDDEN: number,
     INTERNAL_SERVER_ERROR: number,
 } = {
     // Success responses
@@ -15,6 +17,10 @@ export const HTTP_STATUS: {
     // Client error responses
     BAD_REQUEST: 400,
     NOT_FOUND: 404,
+
+    // Authorization
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
 
     // Server error responses
     INTERNAL_SERVER_ERROR: 500,
